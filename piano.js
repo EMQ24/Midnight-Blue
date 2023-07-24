38dc54bb10bbc5181dc3636567ba7ef3a1d1d542
aaronliu0130 <aaronliu0130@gmail.com>
2023-07-24 14:39:34 -0400

    unity

--- piano.js
+++ piano.js
@@ -1,1 +1,46 @@
+let score = 0
+let sequence = []
+let index = 0
 
+//Replace below names with actual names in the html
+let cur = document.getElementById("score")
+let keys = document.getElementsByClassName("key")
+
+//C2 to C7 inclusive     
+//             0123456789
+let symbols = "1!2@34$5%6^78*9(0qQwWeErtTyYuiIoOpPasSdDfgGhHjJklLzZxcCvVbBnm"
+let chords  = "CbDbEFbGbAbBCbDbEFbGbAbBCbDbEFbGbAbBCbDbEFbGbAbBCbDbEFbGbAbBC"
+//number calculation: Math.floor(i/7)+2
+
+let delay = 1300 //2.93 sec is the duration of the files but player migt get inpatient
+let min = 100 //0.1 secs, during testing is still intelligible
+
+//plays the piano key corresponding to symbols[i]
+function play(i) {
+    new Audio('mp3/'+chords[i]+' '+(Math.floor(i/7)+2)+'.mp3').play()
+}
+
+//Displays the sequence
+function display() {
+    index=Math.floor(Math.random()*symbols.length)
+    sequence.push(index)
+    for(i of sequence){
+        flash(i) //TODO: Sashrik add function
+        setTimeout()
+    }
+}
+
+function start() {
+    score=0
+    sequence=[]
+}
+
+
+
+/* Testing index.html code:
+<body>
+    <script>new Audio('mp3/Ab4.mp3').play()
+        setTimeout(function () { new Audio('mp3/Eb6.mp3').play() }, 1300)
+    </script>
+</body>
+*/
